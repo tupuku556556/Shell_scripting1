e3406235d27b47afc51b2fc3cc13c2dd8bdaa220
vijaykumarangajala <44379604+tupuku556556@users.noreply.github.com>
2020-12-22 21:55:31 -0600

    Shell_script1
    Shell_script1

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+echo"Hello Vj"# print
+name=Mark
+echo the name is $name
+echo $BASH
+val10=10
+echo $val10
+echo "Enter Three names"
+read name1 name2 name3
+echo $name1, $name2, $name3
+read -p 'username :' username
+echo 'user name is :' $username
+read -sp 'password:' password
+echo 'password is :' $password
